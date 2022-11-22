f5047239c76931b55d41c7ab55bbb884a41c2aed
LinaW <lina@ustwo.com>
2022-11-22 16:10:25 +0100

    Initial commit

---
+++ ShaderAndVFXGraphURP/Assets/Scripts/EnableGameObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGameObject : MonoBehaviour
+{
+    [SerializeField] private GameObject effect;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            effect.SetActive(!effect.activeSelf);
+        }
+    }
+}
